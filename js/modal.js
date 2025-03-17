cc3b4737cff547f60b5b354675085465135983d2
Людмила Бесащук <besasukludmila@gmail.com>
2025-03-17 12:26:38 +0200

    спроба підключення js до модального вікна

--- js/modal.js
+++ js/modal.js
@@ -7,3 +7,20 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 });
+  let modal = document.getElementById("menuModal");
+        let btn = document.getElementById("openModal");
+        let span = document.getElementsByClassName("close")[0];
+        
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+        
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+        
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
